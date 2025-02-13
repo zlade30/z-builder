2ce96298330535dd519f451dbf01cfd12c4af0ee
zal <zal.dev.30@gmail.com>
2025-02-14 07:29:58 +0800

    setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,26 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({
+        extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+        rules: {
+            semi: ["error"],
+            quotes: ["error", "double"],
+            "prefer-arrow-callback": ["error"],
+            "no-restricted-syntax": [
+                "error",
+                "FunctionExpression",
+                "FunctionDeclaration",
+            ],
+            "@typescript-eslint/no-unused-vars": "warn",
+            curly: ["error", "all"],
+            "no-console": "warn",
+        },
+    }),
 ];
 
 export default eslintConfig;
