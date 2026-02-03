0d94521ac4c0bae23aa6213f0ef22659b64f153a
zee30 <42529336+zlade30@users.noreply.github.com>
2026-02-03 21:26:34 +0800

    Update python-portable.cmd

--- python-portable.cmd
+++ python-portable.cmd
@@ -1,33 +1,74 @@
 @echo off
+setlocal
+
+:: ----------------------------
+:: Configurable variables
+:: ----------------------------
 set PYVER=3.12.1
 set PYDIR=%USERPROFILE%\python-portable
 
-echo Creating folder...
+:: ----------------------------
+:: Create folder
+:: ----------------------------
+echo Creating folder %PYDIR%...
 mkdir "%PYDIR%"
 cd /d "%PYDIR%"
 
-echo Downloading Python embedded...
+:: ----------------------------
+:: Download Python Embedded
+:: ----------------------------
+echo Downloading Python %PYVER% Embedded...
 curl -L https://www.python.org/ftp/python/%PYVER%/python-%PYVER%-embed-amd64.zip -o python.zip
 
-echo Extracting...
+:: ----------------------------
+:: Extract
+:: ----------------------------
+echo Extracting Python...
 tar -xf python.zip
 del python.zip
 
+:: ----------------------------
+:: Enable site-packages
+:: ----------------------------
 echo Enabling site-packages...
 for %%f in (*._pth) do (
-  powershell -NoProfile -Command ^
-    "$f = '%%f'; (Get-Content $f) -replace '#import site','import site' | Set-Content $f"
+    powershell -NoProfile -Command ^
+        "$f = '%%f'; (Get-Content $f) -replace '#import site','import site' | Set-Content $f"
 )
 
-echo Downloading pip installer...
+:: ----------------------------
+:: Add Scripts folder to PATH temporarily
+:: ----------------------------
+set PATH=%PYDIR%;%PYDIR%\Scripts;%PATH%
+
+:: ----------------------------
+:: Download pip installer
+:: ----------------------------
+echo Downloading get-pip.py...
 curl -L https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 
+:: ----------------------------
+:: Install pip
+:: ----------------------------
 echo Installing pip...
-python get-pip.py
+"%PYDIR%\python.exe" get-pip.py --no-warn-script-location
 
-echo Done.
+:: ----------------------------
+:: Cleanup
+:: ----------------------------
+del get-pip.py
+
+:: ----------------------------
+:: Done
+:: ----------------------------
 echo.
+echo Python portable installation complete!
 echo Python location: %PYDIR%
-echo Test with:
-echo   python --version
-echo   pip --version
+echo.
+echo Test Python:
+echo   "%PYDIR%\python.exe" --version
+echo   "%PYDIR%\python.exe" -m pip --version
+echo.
+echo Optional: Add "%PYDIR%" and "%PYDIR%\Scripts" to your PATH to use python and pip globally.
+pause
+endlocal
